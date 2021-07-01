d29f4e40919ec2f4476936c89e3b5ee2a2156f9c
maxdeleon <maxdeleon@vt.edu>
2021-07-01 11:07:51 -0400

    cleaned up main.py example strategy

--- example/main.py
+++ example/main.py
@@ -8,24 +8,15 @@ import random
 Created by Maximo Xavier DeLeon on 6/23/2021
 '''
 
-# create trading algos by utilizing inheritance
+# create custom trading strategies by declaring a Strategy child class
 class dummyBuyStrategy(Strategy):
     def __init__(self):
         Strategy.__init__(self)
 
     def process_1(self):
-        flip = random.randint(1, 2)
         aapl_close = self.asset_dictionary['AAPL'].close # get the current close price of apple stock
         msft_close = self.asset_dictionary['MSFT'].close  # get the current close price of microsoft stock
-        if flip == 1 and aapl_close <= self.trade_cash: # if there is enough cash to buy a share of apple then buy it
-            self.trade('AAPL',1,aapl_close)
-            print(self.current_date,'purchased', 'aapl,', 'remaining cash:', self.cash)
 
-        elif flip == 2 and msft_close <= self.trade_cash:
-            self.trade('MSFT', 1, msft_close)
-            print(self.current_date,'purchased', 'msft,', 'remaining cash:', self.cash)
-        else:
-            print('cash',self.cash)
 
     # more strategy goes here if its beefy
     def process_2(self):
@@ -45,13 +36,11 @@ def main():
     dummy_strategy = dummyBuyStrategy() # define the strategy that will beat the sp500
     engine = alpha.Engine() # define the engine that will test our epic win strat
 
-    engine.backtest(strategy=dummy_strategy, # tell the engine what strategy we want to backtest
-                    data_dict=backtest_data, # tell the engine what data we want to backtest on
-                    starting_cash=25000,# set the starting cash
+    engine.backtest(strategy_object=dummy_strategy,  # tell the engine what strategy we want to backtest
+                    backtest_series_dictionary=backtest_data,  # tell the engine what data we want to backtest on
+                    starting_cash=25000,  # set the starting cash
                     log=True,
                     filename='BACKTEST_LOG.csv')
 
-
 if __name__ == '__main__':
     main()
-
