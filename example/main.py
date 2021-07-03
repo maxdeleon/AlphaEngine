c784669e31777c0627b71323abfa188d675f651f
maxdeleon <maxdeleon@vt.edu>
2021-07-02 21:00:15 -0400

    Moved strategy test into seperate method

--- example/main.py
+++ example/main.py
@@ -25,22 +25,7 @@ class dummyBuyStrategy(Strategy):
         pass
 
 def main():
-    TICKER_LIST = ['AAPL','MSFT','SLV','BNO','SPY']  # define the ticker to trade
-    START_STOP_DATES = ('2020-6-15', '2021-6-15')  # define the start and stop dates
-
-    # get the data
-    backtest_data = yahooClient.get_close_prices_yahoo(tickers=TICKER_LIST, # tickers
-                                                       start_date=START_STOP_DATES[0], # start
-                                                       stop_date=START_STOP_DATES[1]) # stop
-    dummy_strategy = dummyBuyStrategy() # define the strategy that will beat the sp500
-    engine = alpha.Engine() # define the engine that will test our epic win strat
-
-    engine.backtest(strategy_object=dummy_strategy,  # tell the engine what strategy we want to backtest
-                    backtest_series_dictionary=backtest_data,  # tell the engine what data we want to backtest on
-                    starting_cash=25000,  # set the starting cash
-                    log=True,
-                    filename='BACKTEST_LOG.csv')
-
+    #test_algo_class()
     test_stochastic()
 
 def test_stochastic(): # method to test stochastic processes
