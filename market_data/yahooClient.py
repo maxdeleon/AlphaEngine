b2e4882e384fe0f4c983f36ca160ebba08c05bad
maxdeleon <maxdeleon@vt.edu>
2021-07-13 20:31:23 -0400

    removed unused variable

--- market_data/yahooClient.py
+++ market_data/yahooClient.py
@@ -8,7 +8,6 @@ def get_close_prices_yahoo(tickers, start_date, stop_date):
   '''
   asset_dict = {}
   for asset in tickers:
-    df = pd.DataFrame()
     current_asset = yf.Ticker(asset)
     ohclv_bars = current_asset.history(start=start_date, end=stop_date)
     asset_dict[asset] = ohclv_bars
