851a58112df370b4cf3e212ba2d95c03795eb275
maxdeleon <maxdeleon@vt.edu>
2021-07-27 17:27:21 -0400

    Tweaked benchmark strategy class

--- example/main.py
+++ example/main.py
@@ -15,16 +15,28 @@ class BuyAndHold(Strategy):
     def __init__(self):
         Strategy.__init__(self)
         self.has_initialized = False
-        self.allocation_dict = {'SOYB': 0.2,
-                                'UGA': 0.2,
-                                'UNG': 0.2,
-                                'CORN': 0.2}
+        self.verbose = True
+
         # the remaining 20% is just cash
     def build_index(self):
+        self.allocation_dict = {}
+        partition = 1/len(self.asset_dictionary.keys())
         for ticker in self.asset_dictionary.keys():  # for each ticker in the universe of tickers that this algorithm can trade
-            initial_price = self.asset_dictionary[ticker].close
-            initial_quantity = math.floor((self.cash * self.allocation_dict[ticker])/self.asset_dictionary[ticker].close)
-            self.create_order(ticker, initial_quantity, self.asset_dictionary[ticker].close)
+            initial_price = self.asset_dictionary[ticker].bars.Close[0]
+            initial_quantity = math.floor((self.trade_cash * partition)/initial_price)
+            self.allocation_dict[ticker] = {'partition %': str(partition*100)+'%',
+                                            'value':partition*self.trade_cash,
+                                            'quantity':initial_quantity,
+                                            'pps':initial_price}
+        print('portfolio allocation set. creating orders')
+
+        for ticker in self.asset_dictionary.keys():
+            self.create_order(ticker, self.allocation_dict[ticker]['quantity'], self.allocation_dict[ticker]['pps'],message='benchmark order')
+
+
+
+
+
     def process_1(self):
         if not self.has_initialized:  # check to see if this has initialized
             self.build_index()
@@ -53,7 +65,7 @@ def test_algo_class():
 
 def test_stochastic(): # method to test stochastic processes
     print('testing stochastic process classes...') # tell the user what they just did
-    some_asset = {'drift':.2,'volatility':.1,'delta_t':1/255,'initial_price':100} # random parameters
+    some_asset = {'drift':.2,'volatility':.1,'delta_t':1/252,'initial_price':100} # random parameters
     asset_simulator = stoch.StochasticProcessManager(stochastic_parameters=some_asset) # create a instance of the SPM
     simulations = asset_simulator.build_scenarios(amount=100) # generate 10 different GBM simulations
     final_df = pd.DataFrame()
